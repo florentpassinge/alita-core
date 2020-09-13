c3bbb5c9e6fa25d35984370ad9cb2780cb86fb87
florentpassinge <56740211+florentpassinge@users.noreply.github.com>
2020-09-13 14:34:26 +0200

    feat: base admin (#25)
    * feat: base admin
    
    * feat: base admin - menu
    
    * feat: base admin - flash
    
    * feat: base admin - flash
    
    * feat: base admin - topbar
    
    * feat: base admin - topbar
    
    * feat: base admin - content

--- src/Controller/Console/DefaultController.php
+++ src/Controller/Console/DefaultController.php
@@ -2,10 +2,15 @@
 
 namespace Alita\Controller\Console;
 
-use Alita\Controller\BaseController;
+use Alita\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class DefaultController.
@@ -13,13 +18,43 @@ use Symfony\Component\Routing\Annotation\Route;
  * @Route("/alita")
  * @IsGranted("ROLE_ADMIN")
  */
-class DefaultController extends BaseController
+class DefaultController extends AbstractDashboardController
 {
     /**
-     * @Template()
      * @Route("/", name="alita_dashboard")
      */
-    public function index(): void
+    public function index(): Response
     {
+        return parent::index();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::subMenu('Blog', 'fa-page')->setSubItems([
+                MenuItem::linkToUrl('Comments', 'fa fa-comment', '#'),
+                MenuItem::linkToUrl('Users', 'fa fa-user', '#'),
+            ]),
+
+            MenuItem::subMenu('Users', 'fa-cogs')->setSubItems([
+                MenuItem::linkToUrl('Comments', 'fa fa-comment', '#'),
+                MenuItem::linkToUrl('Users', 'fa fa-user', '#'),
+            ]),
+        ];
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        /** @var User $user */
+        $user = clone $user;
+
+        return UserMenu::new()
+            ->setName($user->getName())
+            ->displayUserName(true)
+            ->setAvatarUrl('https://gravatar.com/avatar/59fc9007ea9812447d85cf49909a0ba2?s=32&d=robohash&r=x')
+            ->addMenuItems([
+                MenuItem::linkToRoute('alita.console.profile', 'fa fa-id-card', 'alita_dashboard'),
+                MenuItem::linkToLogout('alita.console.logout', 'fas fa-sign-out-alt'),
+            ]);
     }
 }
